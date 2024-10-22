05af1da7d1d0ccbeb5862e00b4fbd52422dc172f
Rappie <jasper@rappie.com>
2024-10-22 21:22:21 +0200

    Token

--- test/Benchmark.t.sol
+++ test/Benchmark.t.sol
@@ -3,13 +3,25 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor() ERC20("Token", "TKN") {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
+
 contract Benchmark is Test {
+    Token token;
 
     function setUp() public {
+        token = new Token();
     }
 
     function test_benchmark() public {
-        console.log("benchmark");
+        token.mint(address(this), 1000);
+        console.log("token balance: ", token.balanceOf(address(this)));
     }
-
 }
