a601c32006d198220e7393ad260c491f082db0e5
Rappie <jasper@rappie.com>
2024-10-22 21:39:46 +0200

    Add benchmark to Fuzz

--- src/Fuzz.sol
+++ src/Fuzz.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Fuzz {
-    constructor() payable {}
+import "@perimetersec/fuzzlib/src/FuzzBase.sol";
 
-    function benchmark() public payable {
-        assert(false);
+import {Benchmark} from "src/Benchmark.sol";
+
+contract Fuzz is FuzzBase {
+    Benchmark benchmark;
+
+    constructor() payable {
+        benchmark = new Benchmark();
+    }
+
+    function fuzz_benchmark() public payable {
+        uint gasBefore = gasleft();
+        benchmark.run();
+        uint gasAfter = gasleft();
+        fl.log("Gas used", gasBefore - gasAfter);
+
+        fl.t(false, "Benchmark Complete");
     }
 }
